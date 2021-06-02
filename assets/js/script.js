0c86d310113eeb9d0966978b2c00d0d9aef524c3
AndreasChristensen89 <brogaard89@gmail.com>
2021-06-02 15:57:05 +0000

    Complete functions for functional game

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,11 +38,41 @@ function runGame(playerChoice) {
 function calculateWinner() {
     let player = document.getElementById('choice').textContent;
     let computer = document.getElementById('computerChoice').textContent;
-    let result = document.getElementById('outcome').textContent;
-    
-    if(player===computer) {
-        result = "Draw!";
+
+    let rock = ["Spock", "Paper"];
+    let paper = ["Scissors", "Lizard"];
+    let scissors = ["Rock", "Spock"];
+    let lizard = ["Scissors", "Rock"];
+    let spock = ["Paper", "Lizard"];
+
+    if (player===computer) {
+        document.getElementById('outcome').textContent = "Draw!";
+    } 
+    else if ( player==="Rock" && (computer===rock[0] || computer===rock[1]) ) {
+        document.getElementById('outcome').textContent = `${computer} beats ${player} - You lose!`;
+    } 
+    else if( player==="Paper" && (computer===paper[0] || computer===paper[1]) ) {
+        document.getElementById('outcome').textContent = `${computer} beats ${player} - You lose!`;
+    } 
+    else if( player==="Scissors" && (computer===scissors[0] || computer===scissors[1]) ) {
+        document.getElementById('outcome').textContent = `${computer} beats ${player} - You lose!`;
+    } 
+    else if( player==="Lizard" && (computer===lizard[0] || computer===lizard[1]) ) {
+        document.getElementById('outcome').textContent = `${computer} beats ${player} - You lose!`;
+    } 
+    else if( player==="Spock" && (computer===spock[0] || computer===spock[1]) ) {
+        document.getElementById('outcome').textContent = `${computer} beats ${player} - You lose!`;
+    } 
+    else {
+        document.getElementById('outcome').textContent = `${player} beats ${computer} - You win!`;
     }
+
+    if(document.getElementById('outcome').textContent === `${player} beats ${computer} - You win!`) {
+        increaseScore();
+    } else if (document.getElementById('outcome').textContent === `${computer} beats ${player} - You lose!`) {
+        increaseDefeat();
+    }
+
 }
 
 function displayRock() {
@@ -62,10 +92,20 @@ function displaySpock() {
 }
 
 function displayComputerChoice() {
-    let computerChoices = ["Rock", "Paper", "Scissor", "Lizard", "Spock"];
+    let computerChoices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
     let randomNumber = Math.floor(Math.random() * 5);
     let result = computerChoices[randomNumber];
     document.getElementById('computerChoice').textContent = result;
-    calculateWinner(result);
+    calculateWinner();
+}
+
+function increaseScore() {
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
+}
+
+function increaseDefeat() {
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 }
 
