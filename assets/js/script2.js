644873fca28cce63bababee139ba0eb1f1dbfd71
AndreasChristensen89 <brogaard89@gmail.com>
2021-06-03 13:38:05 +0000

    Complete new functions for play and start, work on choice

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-document.getElementById('play-button').addEventListener('click', play);
+document.getElementById("play-button").addEventListener('click', play);
 }
 )
 
@@ -34,35 +34,65 @@ document.getElementById('play-button').addEventListener('click', play);
 
 
 function play() {
-    let body = document.body;
 
     let newSpan = document.createElement('p');
-    newSpan.innerHTML = `Opponent: <span id="opponent"></span>`;
-    let newSpanTwo = document.createElement('p');
-    newSpanTwo.innerHTML = `Chances: <span id="chances"></span>`;
-    let startButton = document.createElement('button');
-    startButton.setAttribute(‘id’,‘start-button’);
-    startButton.setAttribute(‘data-type’,‘start’);
+    newSpan.innerHTML = `Opponent: <span id="opponent">Daiki</span>`;
 
+    let newSpanTwo = document.createElement('p');
+    newSpanTwo.innerHTML = `Chances: <span id="chances">5</span>`;
+    
+    let startButton = document.createElement('button');
+    startButton.innerHTML = `Start`;
+    startButton.setAttribute("id","start-button");
+    startButton.setAttribute("data-type","start");
+
+    let playButton = document.getElementById('play-button');
+
+    playButton.remove();
     document.getElementById('button-area').appendChild(newSpan);
     document.getElementById('button-area').appendChild(newSpanTwo);
     document.getElementById('button-area').appendChild(startButton);
-
-    
-
-
-
-
 
     document.getElementById('start-button').addEventListener('click', start);
 }
 
 function start() {
-    
+    let rock = document.createElement('button');
+    let paper = document.createElement('button');
+    let scissors = document.createElement('button');
+    let lizard = document.createElement('button');
+    let spock = document.createElement('button');
+
+    let startButton = document.getElementById('start-button');
+
+    let buttons = [rock, paper, scissors, lizard, spock];
+    let innerHtml = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+
+    for(let i = 0; i < buttons.length; i++) {
+        buttons[i].setAttribute("data-type", innerHtml[i].toLowerCase());
+        buttons[i].setAttribute("class", "button");
+        buttons[i].innerHTML = innerHtml[i];
+        document.getElementById('button-area').appendChild(buttons[i]);
+        buttons[i].addEventListener('click', function() {
+            let playerChoice = this.getAttribute("data-type");
+            choice(playerChoice);
+        } );
+    }
+    startButton.remove();
 }
 
-function choice() {
+function choice(playerChoice) {
+    document.getElementById('player-choice').value = "";
+    document.getElementById('player-choice').innerHTML = playerChoice;
+    var buttonCheck = document.getElementById("game-area").contains(button);
     
+    if(!buttonCheck) {
+    let goButton = document.createElement('button');
+    goButton.innerHTML = "Go";
+    goButton.setAttribute("class", "button");
+    document.getElementById('game-area').appendChild(goButton);
+    goButton.addEventListener('click', generateComputerChoice);
+    }
 }
 
 function generateComputerChoice() {
