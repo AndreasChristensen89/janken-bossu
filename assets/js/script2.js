364ab63a849383d4ecbc6cebdbb80e5eacd00fbb
AndreasChristensen89 <brogaard89@gmail.com>
2021-06-08 08:28:07 +0000

    Add hamburger nav bar

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -3,6 +3,36 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("play-button").addEventListener('click', play);
 })
 
+// ======================== hamburger nav bar ====================================
+var menu = document.querySelector(".menu")
+var ham = document.querySelector(".ham")
+var xIcon = document.querySelector(".xIcon")
+var menuIcon = document.querySelector(".menuIcon")
+
+ham.addEventListener("click", toggleMenu)
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    xIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    xIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+var menuLinks = document.querySelectorAll(".menuLink")
+
+menuLinks.forEach(
+  function (menuLink) {
+    menuLink.addEventListener("click", toggleMenu)
+  }
+)
+
+// ==================================== game area ====================================
+
 function play() {
     let introText = document.createElement('div');
     introText.innerHTML = `<p>Opponents will get stronger for each round, so watch your health bar. <br>You only need to beat each opponent once to advance to the next level.</p>`
