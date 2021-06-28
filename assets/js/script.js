c25494c21a2967596cd96d0cb9a058701a66c989
AndreasChristensen89 <brogaard89@gmail.com>
2021-06-28 09:15:05 +0000

    Add create-element functions to shorten js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,58 +33,36 @@ menuLinks.forEach(
 
 // ==================================== game area ====================================
 
-function play() {
-    let introText = document.createElement('div');
-    introText.innerHTML = `<p>To play simply pick a hand and press the GO button.<br> Opponents' hit-power will increase for each round, so watch your health bar. <br>You only need to beat each opponent once to advance to the next level.</p>`
-    introText.setAttribute("id", "intro-text");
-
-    let startButton = document.createElement('button');
-    startButton.innerHTML = `START`;
-    startButton.setAttribute("id", "start-button");
-    startButton.setAttribute("data-type", "start");
-
-    let playButton = document.getElementById('play-button');
-
-    playButton.remove();
-    document.getElementById('button-area').appendChild(introText);
-    document.getElementById('button-area').appendChild(startButton);
-    document.getElementById('start-button').addEventListener('click', start);
-
-    let mainIntro = document.getElementById('main-intro');
-    mainIntro.innerHTML = ``;
-    mainIntro.style.height = "unset";
+var dataType = ["rock", "paper", "scissors", "lizard", "spock"];
+var buttonPics = ["url(assets/images/rock.webp)", "url(assets/images/paper.webp)", "url(assets/images/scissors.webp)", "url(assets/images/lizard.webp)", "url(assets/images/spock.webp)"];
+var opponents = ["The Intern", "The Salary Man", "The Manager", "The Yakuza Henchman", "The Biggu Bossu"];
+var backgrounds = ["url(assets/images/intern-div.webp)", "url(assets/images/salaryman-div.webp)", "url(assets/images/manager-div.webp)", "url(assets/images/yakuza-div.webp)", "url(assets/images/biggu-bossu-div.webp)"];
+/**
+ * Used to shorten down the code when creating elements.
+ * Takes four parameters; type of element, id name, the id of element to appendChild, and innerHTML
+ */
+function createElement(type, id, append, html) {
+    let newElement = document.createElement(`${type}`);
+    newElement.setAttribute("id", `${id}`);
+    document.getElementById(`${append}`).appendChild(newElement);
+    newElement.innerHTML = `${html}`;
 }
 
-function start() {
-    document.getElementById('main-intro').remove();
-    document.getElementById('intro-text').remove();
-
-    let newDiv = document.createElement('div');
-    newDiv.innerHTML = `The Intern`;
-    newDiv.setAttribute("id", "opponent");
-
-    let newDivTwo = document.createElement('div');
-    newDivTwo.innerHTML = `100`;
-    newDivTwo.setAttribute("id", "health");
-
-    document.getElementById('button-area').appendChild(newDiv);
-    document.getElementById('button-area').appendChild(newDivTwo);
-
+/**
+ * Creates and inserts the five hand-buttons for playing
+ * No parameters needed
+ */
+function addButtons() {
     let rock = document.createElement('button');
     let paper = document.createElement('button');
     let scissors = document.createElement('button');
     let lizard = document.createElement('button');
     let spock = document.createElement('button');
 
-    let startButton = document.getElementById('start-button');
-    startButton.remove();
-
     let buttons = [rock, paper, scissors, lizard, spock];
-    let dataType = ["rock", "paper", "scissors", "lizard", "spock"];
-    let buttonPics = ["url(assets/images/rock.webp)", "url(assets/images/paper.webp)", "url(assets/images/scissors.webp)", "url(assets/images/lizard.webp)", "url(assets/images/spock.webp)"];
 
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].setAttribute("data-type", dataType[i].toLowerCase());
+        buttons[i].setAttribute("data-type", dataType[i]);
         buttons[i].setAttribute("class", "button");
         buttons[i].style.backgroundImage = `${buttonPics[i]}`;
         document.getElementById('button-area').appendChild(buttons[i]);
@@ -93,6 +71,42 @@ function start() {
             choice(playerChoice);
         });
     }
+}
+
+function removeButtons() {
+    let numOfButtons = document.getElementsByClassName('button');
+
+    for (let i = numOfButtons.length - 1; i >= 0; i--) {
+        numOfButtons[i].remove();
+    }
+
+}
+
+function play() {
+    createElement("div", "intro-text", "button-area", "<p>To play simply pick a hand and press the GO button.<br> Opponents' hit-power will increase for each round, so watch your health bar. <br>You only need to beat each opponent once to advance to the next level.</p>")
+
+    createElement("button", "start-button", "button-area", "START");
+    document.getElementById("start-button").setAttribute("data-type", "start");
+    document.getElementById('start-button').addEventListener('click', start);
+
+    let playButton = document.getElementById('play-button');
+    playButton.remove();
+
+    let mainIntro = document.getElementById('main-intro');
+    mainIntro.innerHTML = ``;
+    mainIntro.style.height = "unset";
+}
+
+function start() {
+    document.getElementById('start-button').remove();
+    document.getElementById('main-intro').remove();
+    document.getElementById('intro-text').remove();
+
+    createElement("div", "opponent", "button-area", "The Intern");
+
+    createElement("div", "health", "button-area", "100");
+
+    addButtons();
 
     let OpponentPic = document.createElement('div');
     OpponentPic.setAttribute("id", "opponent-pic");
@@ -123,30 +137,26 @@ function choice(playerChoice) {
     document.getElementById('outcome').textContent = "";
 
     if (!goArea.contains(button)) {
-        let goButton = document.createElement('button');
-        goButton.innerHTML = `Go`;
-        goButton.setAttribute("id", "go-button");
-        document.getElementById('go-button-area').appendChild(goButton);
-        goButton.addEventListener('click', generateComputerChoice);
+        createElement("button", "go-button", "go-button-area", "Go");
+        document.getElementById("go-button").addEventListener('click', generateComputerChoice);
     }
 }
 
 function generateComputerChoice() {
-    let computerOptions = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+
     let randomNumber = Math.floor(Math.random() * 5);
-    let compResult = computerOptions[randomNumber].toLowerCase();
+    let compResult = dataType[randomNumber];
 
     document.getElementById('go-button').remove();
 
-    let compChoice = document.createElement('div');
+    createElement("div", "comp-hand", "comp-choice", "");
+    let compChoice = document.getElementById('comp-hand');
     let compChoiceStyle = {
         "background-image": `url(assets/images/${compResult}.webp)`,
         "background-size": "contain",
         "background-repeat": "no-repeat",
     };
     Object.assign(compChoice.style, compChoiceStyle);
-    compChoice.setAttribute("id", "comp-hand");
-    document.getElementById('comp-choice').appendChild(compChoice);
 
     calculateWinner(compResult);
 }
@@ -194,7 +204,7 @@ function calculateWinner(compResult) {
                 }
             ], {
                 duration: 500,
-                iterations: 3
+                iterations: 2
             });
             document.getElementById('comp-hand').animate([{
                     transform: 'translateY(0px)'
@@ -207,7 +217,7 @@ function calculateWinner(compResult) {
                 }
             ], {
                 duration: 500,
-                iterations: 3
+                iterations: 2
             });
             let buttons = document.querySelectorAll('.button');
             for (let i = 0; i < buttons.length; i++) {
@@ -225,7 +235,7 @@ function calculateWinner(compResult) {
                 ], {
                     delay: 1500,
                     duration: 1000,
-                    iterations: 3
+                    iterations: 2
                 });
             }
         } else if (playerChoice === failEvents[i].value) {
@@ -288,7 +298,7 @@ function calculateWinner(compResult) {
                         ], {
                             delay: 2500,
                             duration: 1000,
-                            iterations: 3
+                            iterations: 2
                         });
                     }
 
@@ -313,7 +323,6 @@ function calculateWinner(compResult) {
             }
         }
     }
-
 }
 
 function decreaseScore(currentOpponent) {
@@ -363,23 +372,13 @@ function gameOver() {
     document.getElementById('button-area').innerHTML = ``;
     document.getElementById('game-area').innerHTML = ``;
 
-    let lost = document.createElement('h1');
-    lost.innerHTML = `Game Over!`;
-    lost.setAttribute("id", "over-message");
-    document.getElementById('button-area').appendChild(lost);
+    createElement("h1", "over-message", "button-area", "Game Over!");
 
-    let lostMessage = document.createElement('p');
-    lostMessage.innerHTML = `Looks like the odds were against you. Have another go and see if you can reach the top of the coorporate ladder!`;
-    lostMessage.setAttribute("id", "gameover-text");
-    document.getElementById('button-area').appendChild(lostMessage);
+    createElement("p", "gameover-text", "button-area", "Looks like the odds were against you. Have another go and see if you can reach the top of the coorporate ladder!");
 
-    let restartButton = document.createElement('button');
-    restartButton.innerHTML = `Try Again`;
-    restartButton.setAttribute("id", "restart-button");
-    document.getElementById('game-area').appendChild(restartButton);
-    restartButton.addEventListener('click', restart);
+    createElement("button", "restart-button", "game-area", "Try Again");
 
-
+    document.getElementById("restart-button").addEventListener('click', restart);
 }
 
 function restart() {
@@ -391,7 +390,7 @@ function beatOpponent(currentOpponent, playerChoice) {
     document.getElementById('player-choice').style.width = "0";
     document.getElementById('player-choice').style.margin = "0";
     document.getElementById('outcome').style.margin = "0";
-    
+
     let backgroudImg = document.getElementById('comp-hand').style.backgroundImage;
     document.getElementById('comp-hand').remove()
 
@@ -419,50 +418,37 @@ function beatOpponent(currentOpponent, playerChoice) {
     target.parentNode.insertBefore(playerPick, target);
     target.parentNode.insertBefore(compLose, target);
 
-    let opponents = ["The Intern", "The Salary Man", "The Manager", "The Yakuza Henchman", "The Biggu Bossu"];
     let nextIndex = opponents.indexOf(currentOpponent);
 
     document.getElementById('result-area').innerHTML = `<p>You beat ${currentOpponent}.<br> Gear up for the next opponent,<br> ${opponents[nextIndex+1]}!</p>`;
 
-    let nextButton = document.createElement('button');
-    nextButton.innerHTML = `Next Opponent`;
-    nextButton.setAttribute("id", "next-button");
-    document.getElementById('result-area').appendChild(nextButton);
-    nextButton.addEventListener('click', nextOpponent);
+    createElement("button", "next-button", "result-area", "Next Opponent");
+    document.getElementById("next-button").addEventListener('click', nextOpponent);
 
-    displayWinner();
+    removeButtons();
 
     if (currentOpponent === "The Biggu Bossu") {
         beatGame();
     } else {
         let compLeave = document.getElementById('opponent-pic');
         compLeave.animate([{
-            opacity: '1.0'
+                opacity: '1.0'
 
-        },
-        {
-            opacity: '0.0'
+            },
+            {
+                opacity: '0.0'
 
-        }
+            }
 
-    ], {
-        duration: 800,
-        iterations: 1,
-        fill: "forwards"
-    });
+        ], {
+            duration: 800,
+            iterations: 1,
+            fill: "forwards"
+        });
     }
 }
 
-function displayWinner() {
-    let numOfButtons = document.getElementsByClassName('button');
-
-    for (let i = numOfButtons.length - 1; i >= 0; i--) {
-        numOfButtons[i].remove();
-    }
-
-}
-
-function nextOpponent() { 
+function nextOpponent() {
     document.getElementById('player-choice').style = ``;
     document.getElementById('outcome').style = ``;
     document.getElementById('player-pick').remove();
@@ -472,8 +458,6 @@ function nextOpponent() {
     document.getElementById('comp-choice').innerText = "";
     document.getElementById('outcome').innerText = "";
 
-    let backgrounds = ["url(assets/images/intern-div.webp)", "url(assets/images/salaryman-div.webp)", "url(assets/images/manager-div.webp)", "url(assets/images/yakuza-div.webp)", "url(assets/images/biggu-bossu-div.webp)"];
-    let opponents = ["The Intern", "The Salary Man", "The Manager", "The Yakuza Henchman", "The Biggu Bossu"];
     let currentOpponent = document.getElementById('opponent').innerText;
 
     if (currentOpponent === opponents[3]) {
@@ -494,30 +478,12 @@ function nextOpponent() {
         }
     }
 
-    let rock = document.createElement('button');
-    let paper = document.createElement('button');
-    let scissors = document.createElement('button');
-    let lizard = document.createElement('button');
-    let spock = document.createElement('button');
+    addButtons();
 
-    let buttons = [rock, paper, scissors, lizard, spock];
-    let dataType = ["rock", "paper", "scissors", "lizard", "spock"];
-    let buttonPics = ["url(assets/images/rock.webp)", "url(assets/images/paper.webp)", "url(assets/images/scissors.webp)", "url(assets/images/lizard.webp)", "url(assets/images/spock.webp)"];
-
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].setAttribute("data-type", dataType[i]);
-        buttons[i].setAttribute("class", "button");
-        buttons[i].style.backgroundImage = `${buttonPics[i]}`;
-        document.getElementById('button-area').appendChild(buttons[i]);
-        buttons[i].addEventListener('click', function () {
-            let playerChoice = this.getAttribute("data-type");
-            choice(playerChoice);
-        });
-    }
     document.getElementById('health').style.backgroundColor = "#079607";
 
     let compCome;
-    if(document.getElementById('opponent-pic') != null) {
+    if (document.getElementById('opponent-pic') != null) {
         compCome = document.getElementById('opponent-pic');
     } else {
         compCome = document.getElementById('biggu-bossu');
@@ -543,26 +509,12 @@ function beatGame() {
     document.getElementById('game-area').innerHTML = ``;
     document.getElementById('result-area').innerHTML = ``;
 
-    let win = document.createElement('h1');
-    win.innerHTML = `You Win!`;
-    win.setAttribute("id", "win-message");
-    document.getElementById('button-area').appendChild(win);
+    createElement("h1", "win-message", "button-area", "You Win!");
 
-    let winText = document.createElement('h2');
-    winText.innerHTML = `You managed to rise through the coorporate ladder to claim your place on top`;
-    winText.setAttribute("id", "win-text");
-    document.getElementById('game-area').appendChild(winText);
+    createElement("h2", "win-text", "game-area", "You managed to rise through the coorporate ladder to claim your place on top");
 
-    // let winTextTwo = document.createElement('p');
-    // winTextTwo.innerHTML = `Try again and see if you can get a clean run!`;
-    // winTextTwo.setAttribute("id", "win-text-two");
-    // document.getElementById('game-area').appendChild(winTextTwo);
-
-    let restartButton = document.createElement('button');
-    restartButton.innerHTML = `Try Again`;
-    restartButton.setAttribute("id", "restart-button");
-    document.getElementById('game-area').appendChild(restartButton);
-    restartButton.addEventListener('click', restart);
+    createElement("button", "restart-button", "game-area", "Try Again!");
+    document.getElementById("restart-button").addEventListener('click', restart);
 
     document.body.style.backgroundImage = `url(assets/images/background.webp)`;
 
