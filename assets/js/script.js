3bcb1be6cbfa22be5b0ace1c76ade390f837fd30
AndreasChristensen89 <brogaard89@gmail.com>
2021-07-01 12:51:07 +0000

    Separate hamburger nav in file, add css content to css2 file

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,34 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("play-button").addEventListener('click', play);
 })
 
-// ======================== hamburger nav bar ====================================
-var menu = document.querySelector(".menu")
-var ham = document.querySelector(".ham")
-var xIcon = document.querySelector(".xIcon")
-var menuIcon = document.querySelector(".menuIcon")
-
-ham.addEventListener("click", toggleMenu)
-
-function toggleMenu() {
-    if (menu.classList.contains("showMenu")) {
-        menu.classList.remove("showMenu");
-        xIcon.style.display = "none";
-        menuIcon.style.display = "block";
-    } else {
-        menu.classList.add("showMenu");
-        xIcon.style.display = "block";
-        menuIcon.style.display = "none";
-    }
-}
-
-var menuLinks = document.querySelectorAll(".menuLink")
-
-menuLinks.forEach(
-    function (menuLink) {
-        menuLink.addEventListener("click", toggleMenu)
-    }
-)
-
 // ==================================== game area ====================================
 
 var dataType = ["rock", "paper", "scissors", "lizard", "spock"];
